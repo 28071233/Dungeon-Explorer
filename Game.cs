4723c36487ac6153c656b25e23276224bdd5a75e
28071233 <28071233@students.lincoln.ac.uk>
2025-03-06 22:33:04 +0000

    added some character functionality

--- Game.cs
+++ Game.cs
@@ -11,15 +11,26 @@ namespace DungeonExplorer
         public Game()
         {
             // Initialize the game with one room and one player
+            Console.WriteLine($"game function");
+
+            Console.Write("Please enter your characters name: ");
+            string name = Console.ReadLine();
+
+            Player player = new Player(name, 100);
+            Console.WriteLine($"{player.Name}, {player.Health}");
 
         }
+
         public void Start()
         {
             // Change the playing logic into true and populate the while loop
-            bool playing = false;
+            bool playing = true;
             while (playing)
             {
                 // Code your playing logic here
+
+                Console.WriteLine("\nEnd of While loop...");
+                Console.ReadKey();
             }
         }
     }
